8a9ab97406864e185fdad9291c7f0802bbe9d8b6
hari-uc <hariuc2001@gmail.com>
2024-11-30 14:52:18 +0530

    initial commit

---
+++ src/config/db.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'my_database',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'test_database',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+};
