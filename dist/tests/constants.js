a187cf016ead25bda690850c1c58c98e06ec0930
hari-uc <hariuc2001@gmail.com>
2024-12-01 18:02:49 +0530

    added 'dist' folder to .gitignore

--- dist/tests/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = void 0;
-exports.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiY200NWFpZjV4MDAwMDh5MGtmbWVpZGJ6diIsImVtYWlsIjoiaGFyaXVjMjAwMUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IkhhcmkgS3Jpc2huYSIsImlhdCI6MTczMzAzODcyNSwiZXhwIjoxNzMzOTAyNzI1fQ.LsnHGH38OjiJdquGH_Ng6QNxAOeulRXP7-pgBmoedWA";
